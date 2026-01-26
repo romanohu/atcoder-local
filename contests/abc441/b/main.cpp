c70905f803c793a19f47b4d665a2681975b577d3
romanohu <shi301649@gmail.com>
2026-01-26 17:48:06 +0900

    abc441 b c

--- contests/abc441/b/main.cpp
+++ contests/abc441/b/main.cpp
@@ -6,5 +6,38 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int N, M, Q;
+    cin >> N >> M;
+    string S, T;
+    cin >> S >> T;
+    cin >> Q;
+
+    for (int i=0; i<Q; ++i)
+    {
+        string w;
+        cin >> w;
+        while(!w.empty())
+        {
+            char w_front = w.front();
+            w.erase(0, 1);
+            if ((S.find(w_front) != string::npos) && (T.find(w_front) != string::npos))
+            {
+                if (w.empty())
+                {
+                    cout << "Unknown" << "\n";
+                    break;
+                }
+                continue;
+            } else if (S.find(w_front) != string::npos) {
+                cout << "Takahashi" << "\n";
+                break;    
+            } else if (T.find(w_front) != string::npos) {
+                cout << "Aoki" << "\n";
+                break;
+            }
+        }
+    }
+
+
     return 0;
 }
--- contests/abc441/c/main.cpp
+++ contests/abc441/c/main.cpp
@@ -6,5 +6,36 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int N, K;
+    ll X;
+    cin >> N >> K >> X;
+
+    vector<ll> A(N);
+    for (int i=0; i<N; ++i)
+        cin >> A[i];
+    sort(A.begin(), A.end());
+
+    vector<ll> prefix_sum(N+1, 0);
+    for (int i=0; i<N; ++i){
+        prefix_sum[i+1] = prefix_sum[i] + A[i];
+    }
+
+    for (int m=1; m<= N; ++m){
+        int m_sake = m - (N - K);
+
+        if (m_sake <= 0){
+            continue;
+        }
+
+        ll current_sake = prefix_sum[N - m + m_sake] - prefix_sum[N - m];
+
+        if (current_sake >= X){
+            cout << m << "\n";
+            return 0;
+        }
+    }
+
+    cout << -1 << "\n";
+
     return 0;
 }
