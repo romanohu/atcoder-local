64035a5b14fbda98749b9844bec79c502d9e2707
romanohu <shi301649@gmail.com>
2026-02-18 10:41:08 +0900

    abs

--- contests/abs/abc085b/main.cpp
+++ contests/abs/abc085b/main.cpp
@@ -1,5 +1,7 @@
 //#include <bits/stdc++.h>
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 using ll = long long;
 
@@ -7,5 +9,24 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int N;
+    cin >> N;
+    vector<int> D(N,0);
+    for (int i=0; i<N; ++i){
+        cin >> D[i];
+    }
+    sort(D.begin(), D.end());
+    
+    int cnt = 1;
+    int pre = D[0];
+    for (int i=1; i<N; ++i){
+        int now = D[i];
+        if (now == pre)
+            continue;
+        pre = now;
+        cnt++;
+    }
+
+    cout << cnt << "\n";
     return 0;
 }
--- contests/abs/abc085c/main.cpp
+++ contests/abs/abc085c/main.cpp
@@ -7,5 +7,21 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    ll N, Y;
+    cin >> N >> Y;
+
+    for (int z=0; z<=N; ++z){
+        for (int y=0; y<=N-z; ++y){
+            for (int x=0; x<=N-y-z; ++x){
+                ll sum = (10000 * x) + (5000 * y) + (1000 * z);
+                if ((sum == Y) && ((x + y + z) == N)){
+                    cout << x << " " << y << " " << z << "\n";
+                    return 0;
+                }
+            }
+        }
+    }
+
+    cout << "-1 -1 -1" << "\n";
     return 0;
 }
