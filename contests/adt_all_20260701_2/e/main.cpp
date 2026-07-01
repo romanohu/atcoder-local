bb251919c5aa5f07c02f840b8d260bc99f383038
romanohu <shi301649@gmail.com>
2026-07-01 20:12:10 +0900

    adt_all_20260701_2 shokai

--- contests/adt_all_20260701_2/e/main.cpp
+++ contests/adt_all_20260701_2/e/main.cpp
@@ -1,5 +1,6 @@
 //#include <bits/stdc++.h>
 #include <iostream>
+#include <vector>
 using namespace std;
 using ll = long long;
 
@@ -9,6 +10,22 @@ int main() {
 
     int N;
     cin >> N;
+
+    vector<int> cnt(N+1,0), ans;
+
+    for (int i=0; i<N*3; ++i){
+        int c;
+        cin >> c;
+        cnt[c]++;
+        if (cnt[c] == 2)
+            ans.push_back(c);
+    }
+
+    for (int i=0; i<N; ++i){
+        cout << ans[i] << (i == N-1 ? "\n" : " ");
+    }
+
+
 
     
 
